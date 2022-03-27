993acba30efa6f329f204be0966c9aaace8ad58a
evillyf <evillyf05@gmail.com>
2022-03-27 19:23:07 -0300

    criação do menu superior

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,18 +9,39 @@ const HomeScreen = () => {
     return (
         <ScrollView contentContainerStyle={styles.container}>    
             <StatusBar style="auto" translucent={false} backgroundColor={Colors.BASIC_BACKGROUND}/> 
-            <Text>Home Screen</Text>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerTitle}> EM CARTAZ</Text>
+                <Text style={styles.headerSubTitle}> VER TODOS</Text>
+            </View>
         </ScrollView>
         
     );
 };
 
-
+/* STYLE */
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.YELLOW,
+        backgroundColor: Colors.EXTRA_LIGHT_GRAY,  /* definindo a cor de fundo */
     },
+    headerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    
+    headerTitle: {
+        fontSize: 28,
+
+    },
+    headerSubTitle: {
+        fontSize: 13,
+        color: Colors.ACTIVE,
+
+    }
+
 });
 
 export default HomeScreen;
