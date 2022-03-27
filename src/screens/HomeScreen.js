50459affb2d3ee1216841335a782a142a462406e
evillyf <evillyf05@gmail.com>
2022-03-27 19:40:42 -0300

    lista de generos dos filmes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import { StatusBar} from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, ScrollView} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import Colors from "../constants/Colors";
 
+/* definindo a lista de gêneros dos filmes */
+const Genres = ["Todos", "Ação", "Comédia", "Romance", "Horror", "Sci-Fi"]
 
-  /* definindo a barra de notificação - cores no arquivo Colors. styles pode ser dark, light ect */
+/* definindo a barra de notificação - cores no arquivo Colors. styles pode ser dark, light ect */
 const HomeScreen = () => {
     return (
         <ScrollView contentContainerStyle={styles.container}>    
@@ -12,6 +15,11 @@ const HomeScreen = () => {
             <View style={styles.headerContainer}>
                 <Text style={styles.headerTitle}> EM CARTAZ</Text>
                 <Text style={styles.headerSubTitle}> VER TODOS</Text>
+            </View>
+            <View> 
+                <FlatList data= {Genres} horizontal keyExtractor={item => item} renderItem={({item, index}) => {
+                    return <View></View>
+                }}/>
             </View>
         </ScrollView>
         
@@ -31,7 +39,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingVertical: 10,
     },
-    
+
     headerTitle: {
         fontSize: 28,
 
