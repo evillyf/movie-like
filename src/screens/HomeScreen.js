aebbe6e1369cfcd9520eacabc46cc668930d718c
evillyf <evillyf05@gmail.com>
2022-03-27 19:14:33 -0300

    definindo a statusbar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,14 @@
 import { StatusBar} from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, ScrollView} from "react-native";
+import Colors from "../constants/Colors";
 
 
-
+  /* definindo a barra de notificação - cores no arquivo Colors. styles pode ser dark, light ect */
 const HomeScreen = () => {
     return (
-        <ScrollView style={StyleSheet.container}>    
-            <StatusBar style="auto" translucent={false}/>
+        <ScrollView contentContainerStyle={styles.container}>    
+            <StatusBar style="auto" translucent={false} backgroundColor={Colors.BASIC_BACKGROUND}/> 
             <Text>Home Screen</Text>
         </ScrollView>
         
@@ -18,6 +19,7 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: Colors.YELLOW,
     },
 });
 
