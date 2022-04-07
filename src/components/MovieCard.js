cec48c93d71b6672dcfa4702f017e14bdc60f03f
evillyf <evillyf05@gmail.com>
2022-04-06 22:04:51 -0300

    adicionando o banner de filmes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "rect-native";
+import { View, Text, StyleSheet } from "react-native";
 import COLORS from "../constants/Colors";
 
 const MovieCard = () => {
@@ -14,6 +14,11 @@ const MovieCard = () => {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: COLORS.ACTIVE,
+        height: 340,
+        width: 230,
+        borderRadius: 12,
+        elevation: 5,
+        marginVertical: 2,
     },
 });
 
